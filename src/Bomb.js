c26962092b7fe878870322b5f7e2c0fd1b2b656e
Michael Primavera <pastasauce59@gmail.com>
2021-04-28 20:04:05 -0400

    commit tree 940ecec74337990cf7b0871df3f7491c833d5781

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
 // your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+    constructor(props) {
+        super()
+
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    messageHandler = () => {
+        return this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+    }
+
+    render() {
+
+        return <h1>{this.messageHandler()}</h1>
+
+    }
+
+
+
+
+}
+
+export default Bomb;
